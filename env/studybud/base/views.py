98ddeca3f6346f16299de0b0fa8db8b377894dff
JUHIE CHANDRA <juhiechandra@gmail.com>
2022-07-02 20:30:39 +0530

    Rendering variables in Templates

--- env/studybud/base/views.py
+++ env/studybud/base/views.py
@@ -1,9 +1,19 @@
+from multiprocessing import context
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
+
+rooms = [
+    {'id': 1,'name':'Lets learn python!'},
+    {'id': 2,'name':'Discord Club'},
+    {'id': 3,'name':'Lets learn JS!'},
+    {'id': 4,'name':'Lets learn DSA!'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
     return render(request, 'room.html')
